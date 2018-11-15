18bb342892571ca872a2cdafb5db40c591b185ea
Mac Air TP05 <macairtp05@macdemacbook-air.local>
2018-11-15 09:36:39 +0800

    Initial Commit

---
+++ TPSPS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TPSPS
+//
+//  Created by Mac Air TP05 on 2018/11/15.
+//  Copyright © 2018 TP. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
